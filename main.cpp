02fe0673edc65327b3582b80567f87255be614bb
avarxx <valeev.anvar@phystech.edu>
2023-08-29 21:38:59 +0300

    unit_test

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@
 #include "unit_test.h"
 
 
-int main()
+int main(int argc, char** argv)
 {
+    //printf("%s\n", argv[1]);
     const int len_array_data = 3;
 
     bool check = true;
 
     double array_input[len_array_data] = {NAN, NAN, NAN};
+/*
+    int test_run = 0
 
+    scanf("%d", &test_run);
+*/
     int err = Input_coefficients(array_input, check);
 
     if (err < 0)
@@ -28,13 +33,16 @@ int main()
 
     Solve_equations(array_input, &root_1, &root_2, &n_roots);
 
-    if (-EINVAL < 0)
+    if (err < 0)
     {
         printf("array_input == NULL");
     }
 
     Output_roots(n_roots, root_1, root_2, check);
-
+ /*   if (test_run == 1)
+    {
+        Tests();
+    }*/
     Tests();
 
     return 0;
